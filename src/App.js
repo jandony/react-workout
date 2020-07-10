832c1c879b55c7fe520c7f9ab276d907e2b06fe4
Greggles <gregory@wetransfer.com>
2020-07-10 10:37:14 +0200

    Move Navbar to component and add your workout tab

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 import AppBar from "@material-ui/core/AppBar";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import SearchIcon from "@material-ui/icons/Search";
-import CreateIcon from "@material-ui/icons/Create";
+
+//
+import NavBar from "./Components/NavBar/NavBar";
 
 // view components
 import ExerciseFinder from "./Views/ExerciseFinder.jsx";
@@ -36,26 +34,56 @@ const App = () => {
   };
 
   const [exercises, setExercises] = useState([
-    { name: "Bench Press", description: "Bench Press description example", body: "upper" },
-    { name: "Bicep Curls", description: "Bicep Curls description example", body: "upper" },
-    { name: "Chest Press", description: "Chest Press description example", body: "upper" },
-    { name: "Push Ups", description: "Push Ups description example", body: "upper" },
-    { name: "Calf Raises", description: "Calf Raises description example", body: "lower" },
-    { name: "Deadlifts", description: "Deadlifts description example", body: "lower" },
-    { name: "Lunges", description: "Lunges description example", body: "lower" },
-    { name: "Squats", description: "Squats description example", body: "lower" },
+    {
+      name: "Bench Press",
+      description: "Bench Press description example",
+      body: "upper",
+    },
+    {
+      name: "Bicep Curls",
+      description: "Bicep Curls description example",
+      body: "upper",
+    },
+    {
+      name: "Chest Press",
+      description: "Chest Press description example",
+      body: "upper",
+    },
+    {
+      name: "Push Ups",
+      description: "Push Ups description example",
+      body: "upper",
+    },
+    {
+      name: "Calf Raises",
+      description: "Calf Raises description example",
+      body: "lower",
+    },
+    {
+      name: "Deadlifts",
+      description: "Deadlifts description example",
+      body: "lower",
+    },
+    {
+      name: "Lunges",
+      description: "Lunges description example",
+      body: "lower",
+    },
+    {
+      name: "Squats",
+      description: "Squats description example",
+      body: "lower",
+    },
   ]);
   return (
-    <div className="App" style={{ backgroundColor: "lightgrey", minHeight: "98vh" }}>
+    <div
+      className="App"
+      style={{ backgroundColor: "lightgrey", minHeight: "98vh" }}
+    >
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <AppBar className={classes.appBar}>
-            <Paper style={{ borderRadius: 0 }}>
-              <Tabs value={value} onChange={handleChange} indicatorColor="primary" textColor="primary" centered>
-                <Tab label="Exercise Finder" icon={<SearchIcon />} component={Link} to="/" onClick={handleChange} />
-                <Tab label="Record Workout" icon={<CreateIcon />} component={Link} to="/record" onClick={handleChange} />
-              </Tabs>
-            </Paper>
+            <NavBar />
           </AppBar>
           <Switch>
             <Route exact path="/" component={ExerciseFinder} />
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
@@ -6,6 +8,7 @@ import Tab from "@material-ui/core/Tab";
 
 import SearchIcon from "@material-ui/icons/Search";
 import CreateIcon from "@material-ui/icons/Create";
+import FitnessCenter from "@material-ui/icons/FitnessCenter";
 
 const useStyles = makeStyles({
   root: {
@@ -22,10 +25,35 @@ export default function CenteredTabs() {
   };
 
   return (
-    <Paper className={classes.root}>
-      <Tabs value={value} onChange={handleChange} indicatorColor="primary" textColor="primary" centered>
-        <Tab label="Exercise Finder" icon={<SearchIcon />} />
-        <Tab label="Record Workout" icon={<CreateIcon />}></Tab>
+    <Paper style={{ borderRadius: 0 }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab
+          label="Exercise Finder"
+          icon={<SearchIcon />}
+          component={Link}
+          to="/"
+          onClick={handleChange}
+        />
+        <Tab
+          label="Record Workout"
+          icon={<CreateIcon />}
+          component={Link}
+          to="/record"
+          onClick={handleChange}
+        />
+        <Tab
+          label="Your Workouts"
+          icon={<FitnessCenter />}
+          component={Link}
+          to="/workouts"
+          onClick={handleChange}
+        />
       </Tabs>
     </Paper>
   );

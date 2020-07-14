37116469220cd1dbe1ad03bbbb046339d4c83237
Greggles <gregory@wetransfer.com>
2020-07-14 18:15:56 +0200

    Extracted exercises database and refactored to use a class

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import AppBar from "@material-ui/core/AppBar";
 
-//
+import { upperExercises, lowerExercises } from "./Data";
+
 import NavBar from "./Components/NavBar/NavBar";
 
 // view components
@@ -22,23 +23,6 @@ import history from "./History";
 import { ThemeProvider } from "@material-ui/core";
 import theme from "./theme";
 import "./App.css";
-
-// body images
-import Neck from "./Assets/Images/neck.jpg";
-import Traps from "./Assets/Images/traps.jpg";
-import Shoulders from "./Assets/Images/shoulders.jpg";
-import Chest from "./Assets/Images/chest.jpg";
-import Abs from "./Assets/Images/abs.jpg";
-import Lats from "./Assets/Images/lats.jpg";
-import Back from "./Assets/Images/back.jpg";
-import Biceps from "./Assets/Images/biceps.jpg";
-import Triceps from "./Assets/Images/triceps.jpg";
-import Forearms from "./Assets/Images/forearms.jpg";
-
-import Glutes from "./Assets/Images/glutes.jpg";
-import Quads from "./Assets/Images/quads.jpg";
-import Hamstrings from "./Assets/Images/hamstrings.jpg";
-import Calves from "./Assets/Images/calves.jpg";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -58,137 +42,8 @@ const App = () => {
   const [selectedExercises, setSelectedExercises] = useState([]);
   const [totalExercises, setTotal] = useState(0);
 
-  const [upperWorkouts, setUpperWorkouts] = useState([
-    {
-      name: "Neck",
-      body: "upper",
-      exercises: [
-        { name: "Neck Ex 1", image: Neck },
-        { name: "Neck Ex 2", image: Neck },
-      ],
-      image: Neck,
-    },
-    {
-      name: "Traps (trapezius)",
-      body: "upper",
-      exercises: [
-        { name: "Traps Ex 1", image: Traps },
-        { name: "Traps Ex 2", image: Traps },
-      ],
-      image: Traps,
-    },
-    {
-      name: "Shoulders (deltoids)",
-      body: "upper",
-      exercises: [
-        { name: "Shoulders Ex 1", image: Shoulders },
-        { name: "Shoulders Ex 2", image: Shoulders },
-      ],
-      image: Shoulders,
-    },
-    {
-      name: "Chest (pectoralis",
-      body: "upper",
-      exercises: [
-        { name: "Chest Ex 1", image: Chest },
-        { name: "Chest Ex 2", image: Chest },
-      ],
-      image: Chest,
-    },
-    {
-      name: "Abs (abdominis)",
-      body: "upper",
-      exercises: [
-        { name: "Abs Ex 1", image: Abs },
-        { name: "Abs Ex 2", image: Abs },
-      ],
-      image: Abs,
-    },
-    {
-      name: "Lats (latissimus dorsi)",
-      body: "upper",
-      exercises: [
-        { name: "Lats Ex 1", image: Lats },
-        { name: "Lats Ex 2", image: Lats },
-      ],
-      image: Lats,
-    },
-    {
-      name: "Middle Back",
-      body: "upper",
-      exercises: [
-        { name: "Middle Back Ex 1", image: Back },
-        { name: "Middle Back Ex 2", image: Back },
-      ],
-      image: Back,
-    },
-    {
-      name: "Lower Back",
-      body: "upper",
-      exercises: [
-        { name: "Lower Back Ex 1", image: Back },
-        { name: "Lower Back Ex 2", image: Back },
-      ],
-      image: Back,
-    },
-    {
-      name: "Biceps",
-      body: "upper",
-      exercises: [
-        { name: "Biceps Ex 1", image: Biceps },
-        { name: "Biceps Ex 2", image: Biceps },
-      ],
-      image: Biceps,
-    },
-    {
-      name: "Triceps",
-      body: "upper",
-      exercises: [
-        { name: "Triceps Ex 1", image: Triceps },
-        { name: "Triceps Ex 2", image: Triceps },
-      ],
-      image: Triceps,
-    },
-    {
-      name: "Forearms",
-      body: "upper",
-      exercises: [
-        { name: "Forearms Ex 1", image: Forearms },
-        { name: "Forearms Ex 2", image: Forearms },
-      ],
-      image: Forearms,
-    },
-  ]);
-
-  const [lowerWorkouts, setLowerWorkouts] = useState([
-    {
-      name: "Glutes (gluteus maximus)",
-      body: "lower",
-      exercises: [{ name: "Glutes Ex 1" }, { name: "Glutes Ex 2" }],
-      image: Glutes,
-    },
-    {
-      name: "Quads (quadriceps)",
-      body: "lower",
-      exercises: [{ name: "Quads Ex 1" }, { name: "Quads Ex 2" }],
-      image: Quads,
-    },
-    {
-      name: "Hamstrings",
-      body: "lower",
-      exercises: [{ name: "Hamstrings Ex 1" }, { name: "Hamstrings Ex 2" }],
-      image: Hamstrings,
-    },
-    {
-      name: "Calves",
-      body: "lower",
-      exercises: [
-        { name: "Calves Ex 1", image: Calves },
-        { name: "Calves Ex 2", image: Calves },
-      ],
-      image: Calves,
-    },
-  ]);
+  const [upperWorkouts, setUpperWorkouts] = useState(upperExercises);
+  const [lowerWorkouts, setLowerWorkouts] = useState(lowerExercises);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
